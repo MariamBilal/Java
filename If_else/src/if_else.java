a778656ccc73739bb5a442da2d2081b6f5434bb5
MariamBilal <mariambilal6@gmail.com>
2018-11-12 04:45:14 +0500

    initial commit

---
+++ If_else/src/if_else.java
@@ -0,0 +1,18 @@
+
+public class if_else{
+
+	public static void main(String[] args){
+		
+		 int num=20;
+	      if( num < 50 ){
+		  
+		  System.out.println("number is less than 50");
+	      
+	      }
+	  
+	      else {
+	    		System.out.println("num is greater than or equal 50");
+	    	     }
+	}
+
+}
---
+++ If_else/src/nestedIf.java
@@ -0,0 +1,15 @@
+
+public class nestedIf {
+
+	public static void main(String[] args) {
+		int num = 60;
+		if( num < 100 ){ 
+	           System.out.println("number is less than 100"); 
+	           if(num > 50){
+		      System.out.println("number is greater than 50");
+		   }
+		}
+
+	}
+
+}
