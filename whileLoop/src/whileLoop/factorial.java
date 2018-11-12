19ac9703407489b346d4f9f516cc7afbdbef0253
MariamBilal <mariambilal6@gmail.com>
2018-11-12 05:33:31 +0500

    initial commit

---
+++ whileLoop/src/whileLoop/factorial.java
@@ -0,0 +1,16 @@
+package whileLoop;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		 int number = 6;
+	        long fact = 1;
+	        for(int i = 1; i <= number; i++)
+	        {
+	            fact = fact * i;
+	        }
+	        System.out.println("Factorial of "+number+" is: "+fact);
+
+	}
+
+}
---
+++ whileLoop/src/whileLoop/whileloops.java
@@ -0,0 +1,28 @@
+package whileLoop;
+
+public class whileloops {
+
+	public static void main(String[] args) {
+		int i=10;
+        while(i>1)
+        {
+            System.out.println(i);
+             i--;
+        }
+        
+        System.out.println("-----------------");
+        
+        int arr[] = {2,11,45,9};
+        
+        int j =0;
+        
+        
+        while(j<4){
+             System.out.println(arr[j]);
+             j++;
+        }
+       
+
+	}
+
+}
